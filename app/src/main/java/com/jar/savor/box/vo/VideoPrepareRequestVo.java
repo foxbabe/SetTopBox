3e43bbae080d299c40a765ce20dfef672b6d4e56
zhanghq <zhang.hq@hotmail.com>
2017-03-14 19:05:44 +0800

    remould done

---
+++ app/src/main/java/com/jar/savor/box/vo/VideoPrepareRequestVo.java
@@ -0,0 +1,17 @@
+package com.jar.savor.box.vo;
+
+/**
+ * Created by zhanghq on 2017/3/14.
+ */
+
+public class VideoPrepareRequestVo {
+    private String mediaPath;
+
+    public String getMediaPath() {
+        return mediaPath;
+    }
+
+    public void setMediaPath(String mediaPath) {
+        this.mediaPath = mediaPath;
+    }
+}
