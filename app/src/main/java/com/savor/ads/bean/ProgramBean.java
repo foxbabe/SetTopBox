968d16d6eb7d0db56b6c8620ced78fef22c086d7
bichao1986 <kaqi_123@126.com>
2017-10-16 10:12:25 +0800

    111

---
+++ app/src/main/java/com/savor/ads/bean/ProgramBean.java
@@ -0,0 +1,8 @@
+package com.savor.ads.bean;
+
+/**
+ * Created by Administrator on 2017/10/12.
+ */
+
+public class ProgramBean {
+}
---
+++ app/src/main/java/com/savor/ads/bean/ProgramBeanResult.java
@@ -0,0 +1,11 @@
+package com.savor.ads.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2017/10/12.
+ */
+
+public class ProgramBean implements Serializable{
+    
+}
