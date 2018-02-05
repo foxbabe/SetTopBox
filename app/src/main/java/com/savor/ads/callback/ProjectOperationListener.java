cbf9fc15e186f7e223461de35af95a5a01f2023b
zhanghaiqiang <zhangh.hq@hotmail.com>
2018-02-05 10:02:52 +0800

    123

--- app/src/main/java/com/savor/ads/callback/ProjectOperationListener.java
+++ app/src/main/java/com/savor/ads/callback/ProjectOperationListener.java
@@ -73,131 +73,6 @@ public class ProjectOperationListener implements OnRemoteOperationListener {
         mContext = context;
     }
 
-//    @Override
-//    public PrepareResponseVo prepare(PrepareRequestVo prepareRequestVo) {
-//        PrepareResponseVo localResult = new PrepareResponseVo();
-//
-//        if (prepareRequestVo == null) {
-//            localResult.setResult(ConstantValues.SERVER_RESPONSE_CODE_FAILED);
-//            localResult.setInfo("参数为空！");
-//        } else {
-//
-//            String url = prepareRequestVo.getAsseturl();
-//            String assetType = prepareRequestVo.getAssettype();
-//            String type = assetType;
-//            String vid = "";
-//
-//            boolean vodCheckPass = true;
-//
-//            if (assetType.equals(ConstantValues.PROJECT_TYPE_VIDEO)) {
-//                if (prepareRequestVo.getAction().equals(ConstantValues.PROJECT_TYPE_VIDEO_VOD)) {
-//                    type = ConstantValues.PROJECT_TYPE_VIDEO_VOD;
-//                    // 视频标题
-//                    url = prepareRequestVo.getAssetname();
-//                    DBHelper dbHelper = DBHelper.get(mContext);
-//                    if (prepareRequestVo.getVodType() == 2) {
-//                        // 酒楼宣传片点播
-//                        List<PlayListBean> list = dbHelper.findPlayListByWhere(DBHelper.MediaDBInfo.FieldName.MEDIANAME + "=?", new String[]{url});
-//
-//                        if (list != null && !list.isEmpty()) {
-//                            PlayListBean bean = list.get(0);
-//                            String filePath = AppUtils.getFilePath(mContext, AppUtils.StorageFile.media) + bean.getMedia_name();
-//                            String md5 = bean.getMd5();
-//                            File file = new File(filePath);
-//                            if (file.exists()) {
-//                                String vodMd5 = AppUtils.getEasyMd5(file);
-//                                if (!vodMd5.equals(md5)) {
-////                                    file.delete();
-////                                    dbHelper.deleteDataByWhere(DBHelper.MediaDBInfo.TableName.PLAYLIST,
-////                                            DBHelper.MediaDBInfo.FieldName.MEDIANAME + "=?", new String[]{url});
-//
-//                                    localResult.setInfo("该点播视频无法播放，请稍后再试");
-//                                    vodCheckPass = false;
-//                                }
-//                            } else {
-//                                localResult.setInfo("没有找到点播视频！");
-//                                vodCheckPass = false;
-//                            }
-//
-//                            url = filePath;
-//                            vid = bean.getVid();
-//                        } else {
-//                            localResult.setInfo("没有找到点播视频！");
-//                            vodCheckPass = false;
-//                        }
-//                    } else {
-//                        // 普通点播视频点播
-//                        List<OnDemandBean> list = dbHelper.findMutlicastMediaLibByWhere(DBHelper.MediaDBInfo.FieldName.TITLE + "=?", new String[]{url});
-//
-//                        if (list != null && !list.isEmpty()) {
-//                            OnDemandBean bean = list.get(0);
-//                            String filePath = AppUtils.getFilePath(mContext, AppUtils.StorageFile.multicast) + bean.getTitle();
-//                            String md5 = bean.getMd5();
-//                            File file = new File(filePath);
-//                            if (file.exists()) {
-//                                String vodMd5 = AppUtils.getEasyMd5(file);
-//                                if (!vodMd5.equals(md5)) {
-//                                    file.delete();
-//                                    dbHelper.deleteDataByWhere(DBHelper.MediaDBInfo.TableName.MULTICASTMEDIALIB,
-//                                            DBHelper.MediaDBInfo.FieldName.TITLE + "=?", new String[]{url});
-//
-//                                    localResult.setInfo("该点播视频无法播放，请稍后再试");
-//                                    vodCheckPass = false;
-//                                }
-//                            } else {
-//                                localResult.setInfo("没有找到点播视频！");
-//                                vodCheckPass = false;
-//                            }
-//
-//                            url = filePath;
-//                            vid = bean.getVodId();
-//                        } else {
-//                            localResult.setInfo("没有找到点播视频！");
-//                            vodCheckPass = false;
-//                        }
-//                    }
-//
-//
-////                    dbHelper.close();
-//                } else {
-//                    type = ConstantValues.PROJECT_TYPE_VIDEO;
-//                }
-//            }
-//
-//            if (vodCheckPass) {
-//                localResult.setResult(ConstantValues.SERVER_RESPONSE_CODE_SUCCESS);
-//                localResult.setInfo("加载成功！");
-//
-//                // 跳转或将参数设置到ScreenProjectionActivity
-//                Bundle data = new Bundle();
-//                data.putString(ScreenProjectionActivity.EXTRA_URL, url);
-//                data.putString(ScreenProjectionActivity.EXTRA_TYPE, type);
-//                data.putString(ScreenProjectionActivity.EXTRA_MEDIA_ID, vid);
-//                Activity activity = ActivitiesManager.getInstance().getCurrentActivity();
-//                if (activity instanceof ScreenProjectionActivity && !((ScreenProjectionActivity) activity).isBeenStopped()) {
-//                    LogUtils.d("Listener will setNewProjection");
-//                    ((ScreenProjectionActivity) activity).setNewProjection(data);
-//                } else {
-//                    if (ActivitiesManager.getInstance().getCurrentActivity() == null) {
-//                        LogUtils.d("Listener will startActivity in new task");
-//                        Intent intent = new Intent(mContext, ScreenProjectionActivity.class);
-//                        intent.putExtras(data);
-//                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                        mContext.startActivity(intent);
-//                    } else {
-//                        LogUtils.d("Listener will startActivity in " + activity);
-//                        Intent intent = new Intent(activity, ScreenProjectionActivity.class);
-//                        intent.putExtras(data);
-//                        activity.startActivity(intent);
-//                    }
-//                }
-//            } else {
-//                localResult.setResult(ConstantValues.SERVER_RESPONSE_CODE_FAILED);
-//            }
-//        }
-//        return localResult;
-//    }
-
     @Override
     public PrepareResponseVoNew showVod(String mediaName, String vodType, int position, boolean isFromWeb, boolean isNewDevice) {
         PrepareResponseVoNew localResult = new PrepareResponseVoNew();
@@ -406,30 +281,6 @@ public class ProjectOperationListener implements OnRemoteOperationListener {
         return localResult;
     }
 
-//    /**
-//     * 调整播放进度
-//     *
-//     * @param position
-//     * @return
-//     */
-//    @Override
-//    public SeekResponseVo seek(int position) {
-////        Activity activity = ActivitiesManager.getInstance().getCurrentActivity();
-////        if (activity instanceof ScreenProjectionActivity) {
-////            return ((ScreenProjectionActivity) activity).seekTo(position);
-////        } else {
-////            return null;
-////        }
-//
-//        SeekAction seekAction = new SeekAction(position);
-//        ProjectionManager.getInstance().enqueueAction(seekAction);
-//
-//        SeekResponseVo responseVo = new SeekResponseVo();
-//        responseVo.setResult(ConstantValues.SERVER_RESPONSE_CODE_SUCCESS);
-//        responseVo.setInfo("成功");
-//        return responseVo;
-//    }
-
     @Override
     public SeekResponseVo seek(int position, String projectId) {
 //        Activity activity = ActivitiesManager.getInstance().getCurrentActivity();
@@ -456,21 +307,6 @@ public class ProjectOperationListener implements OnRemoteOperationListener {
 //            return responseVo;
 //        }
     }
-
-//    /**
-//     * 暂停、恢复播放
-//     *
-//     * @return
-//     */
-//    @Override
-//    public PlayResponseVo play(int action) {
-//        Activity activity = ActivitiesManager.getInstance().getCurrentActivity();
-//        if (activity instanceof ScreenProjectionActivity) {
-//            return ((ScreenProjectionActivity) activity).togglePlay(action);
-//        } else {
-//            return null;
-//        }
-//    }
 
     @Override
     public PlayResponseVo play(int action, String projectId) {
@@ -499,26 +335,6 @@ public class ProjectOperationListener implements OnRemoteOperationListener {
 //            return responseVo;
 //        }
     }
-
-//    /**
-//     * 停止投屏
-//     *
-//     * @return
-//     */
-//    @Override
-//    public StopResponseVo stop() {
-//        Activity activity = ActivitiesManager.getInstance().getCurrentActivity();
-//        if (activity instanceof ScreenProjectionActivity) {
-//            ((ScreenProjectionActivity) activity).stop(true);
-//
-//            StopResponseVo stopResponseVo = new StopResponseVo();
-//            stopResponseVo.setResult(ConstantValues.SERVER_RESPONSE_CODE_SUCCESS);
-//
-//            return stopResponseVo;
-//        } else {
-//            return null;
-//        }
-//    }
 
     @Override
     public StopResponseVo stop(String projectId) {
@@ -600,22 +416,6 @@ public class ProjectOperationListener implements OnRemoteOperationListener {
         ProjectionManager.getInstance().enqueueAction(stopAction);
     }
 
-    /**
-     * 旋转图片
-     *
-     * @param rotateDegree
-     * @return
-     */
-//    @Override
-//    public RotateResponseVo rotate(int rotateDegree) {
-//        Activity activity = ActivitiesManager.getInstance().getCurrentActivity();
-//        if (activity instanceof ScreenProjectionActivity) {
-//            return ((ScreenProjectionActivity) activity).rotate(rotateDegree);
-//        } else {
-//            return null;
-//        }
-//    }
-
     @Override
     public RotateResponseVo rotate(int rotateDegree, String projectId) {
 //        Activity activity = ActivitiesManager.getInstance().getCurrentActivity();
@@ -645,19 +445,6 @@ public class ProjectOperationListener implements OnRemoteOperationListener {
 //        }
     }
 
-//    @Override
-//    public VolumeResponseVo volume(int action) {
-//        Activity activity = ActivitiesManager.getInstance().getCurrentActivity();
-//        if (activity instanceof ScreenProjectionActivity) {
-//            return ((ScreenProjectionActivity) activity).volume(action);
-//        } else {
-//            // 不在ScreenProjectionActivity页面时认为已经播放完毕结束ScreenProjectionActivity了
-//            VolumeResponseVo responseVo = new VolumeResponseVo();
-//            responseVo.setResult(ConstantValues.SERVER_RESPONSE_CODE_FAILED);
-//            return responseVo;
-//        }
-//    }
-
     @Override
     public VolumeResponseVo volume(int action, String projectId) {
 //        Activity activity = ActivitiesManager.getInstance().getCurrentActivity();
@@ -684,24 +471,6 @@ public class ProjectOperationListener implements OnRemoteOperationListener {
 //            return responseVo;
 //        }
     }
-
-    /**
-     * 点播者查询播放进度等信息
-     *
-     * @return
-     */
-//    @Override
-//    public Object query() {
-//        Activity activity = ActivitiesManager.getInstance().getCurrentActivity();
-//        if (activity instanceof ScreenProjectionActivity) {
-//            return ((ScreenProjectionActivity) activity).query();
-//        } else {
-//            // 不在ScreenProjectionActivity页面时认为已经播放完毕结束ScreenProjectionActivity了
-//            QueryPosBySessionIdResponseVo queryResponse = new QueryPosBySessionIdResponseVo();
-//            queryResponse.setResult(ConstantValues.SERVER_RESPONSE_CODE_VIDEO_COMPLETE);
-//            return queryResponse;
-//        }
-//    }
 
     @Override
     public Object query(String projectId) {
